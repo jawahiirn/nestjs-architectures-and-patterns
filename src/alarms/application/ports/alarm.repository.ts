a0a99f267237d2974f39789b557a3ab9f4a4aa45
Nabhan <jawahiirboss@gmail.com>
2026-02-28 00:27:43 +0600

    Infrastructure layer: orm & in-memory data persistence.

--- src/alarms/application/ports/alarm.repository.ts
+++ src/alarms/application/ports/alarm.repository.ts
@@ -1,6 +1,6 @@
 import { Alarm } from '../../domain/alarm';
 
 export abstract class AlarmRepository {
-  abstract findAll(): Promise<Alarm[]>;
-  abstract save(alarm: Alarm): Promise<Alarm>;
+  abstract findAll(): Promise<Alarm[]> | Alarm[];
+  abstract save(alarm: Alarm): Promise<Alarm> | Alarm;
 }
