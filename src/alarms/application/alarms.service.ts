007eb4449ead41321b01c244222cb117a6516046
Nabhan <jawahiirboss@gmail.com>
2026-03-06 04:20:09 +0600

    we created a create-command handler and modified the service to use the command handler's execute method through command bus.

--- src/alarms/application/alarms.service.ts
+++ src/alarms/application/alarms.service.ts
@@ -1,20 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAlarmCommand } from './commands/create-alarm.command';
 import { AlarmRepository } from './ports/alarm.repository';
-import { AlarmFactory } from '../domain/factories/alarm.factory';
+import { CommandBus } from '@nestjs/cqrs';
 
 @Injectable()
 export class AlarmsService {
   constructor(
     private readonly alarmRepository: AlarmRepository,
-    private alarmFactory: AlarmFactory,
+    private readonly commandBus: CommandBus,
   ) {}
   create(createAlarmCommand: CreateAlarmCommand) {
-    const alarm = this.alarmFactory.create(
-      createAlarmCommand.name,
-      createAlarmCommand.severity,
-    );
-    return this.alarmRepository.save(alarm);
+    return this.commandBus.execute(createAlarmCommand);
   }
 
   findAll() {
