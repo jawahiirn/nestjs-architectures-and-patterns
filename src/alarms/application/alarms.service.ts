f774981d7a0b465428814dd2bdb4ceddd547ab3d
Nabhan <jawahiirboss@gmail.com>
2026-03-06 04:49:51 +0600

    we created a query-command handler and modified the service to use the command handler's execute method through query bus for READ operations from DB.

--- src/alarms/application/alarms.service.ts
+++ src/alarms/application/alarms.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAlarmCommand } from './commands/create-alarm.command';
-import { AlarmRepository } from './ports/alarm.repository';
-import { CommandBus } from '@nestjs/cqrs';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { GetAlarmsQuery } from './queries/get-alarms.query';
 
 @Injectable()
 export class AlarmsService {
   constructor(
-    private readonly alarmRepository: AlarmRepository,
     private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
   ) {}
   create(createAlarmCommand: CreateAlarmCommand) {
     return this.commandBus.execute(createAlarmCommand);
   }
 
   findAll() {
-    return this.alarmRepository.findAll();
+    return this.queryBus.execute(new GetAlarmsQuery());
   }
 }
