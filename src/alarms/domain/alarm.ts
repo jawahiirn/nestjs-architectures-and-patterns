63832bebc9fe01094d0ae30253f5177fa8840bb6
Nabhan <jawahiirboss@gmail.com>
2026-03-06 15:14:05 +0600

    - fix the alarm factory assignments in changes for the domain models - orm alarm mappers updated.

--- src/alarms/domain/alarm.ts
+++ src/alarms/domain/alarm.ts
@@ -1,9 +1,19 @@
 import { AlarmSeverity } from './value-objects/alarm-severity';
+import { AlarmItem } from './alarm-item';
 
 export class Alarm {
-  constructor(
-    public id: string,
-    public name: string,
-    public severity: AlarmSeverity,
-  ) {}
+  public name: string;
+  public severity: AlarmSeverity;
+  public triggeredAt: Date;
+  public isAcknowledged: boolean = false;
+  public items: Array<AlarmItem>;
+  constructor(public id: string) {}
+
+  acknowledge() {
+    this.isAcknowledged = true;
+  }
+
+  addAlarmItem(item: AlarmItem) {
+    this.items.push(item);
+  }
 }
